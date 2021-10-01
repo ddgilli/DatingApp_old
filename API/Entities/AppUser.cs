b0de9764aeee267f69a5f2f989529d4eb71b1874
ddgilli <35736012+ddgilli@users.noreply.github.com>
2021-10-01 16:13:58 -0400

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 namespace API.Entities
 {
     public class AppUser
-    {
+    {    
+        
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
     
 }
