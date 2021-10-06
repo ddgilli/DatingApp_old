9f638ba68588567966767ff0d9914ebeea43ab28
ddgilli <35736012+ddgilli@users.noreply.github.com>
2021-10-06 09:29:04 -0400

    End of section 6

--- API/client/src/app/_services/account.service.ts
+++ API/client/src/app/_services/account.service.ts
@@ -15,7 +15,7 @@ export class AccountService {
   constructor(private http: HttpClient) { }
 
   login(model: User) {
-    return this.http.post<User>(this.baseUrl + 'account/login',model).pipe(
+    return this.http.post(this.baseUrl + 'account/login',model).pipe(
       map((response: User) => {
         const user = response;
         if (user) {
