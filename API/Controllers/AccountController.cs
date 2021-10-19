2c6e1b400b23cdebd02232a44b6f87e2c80ddc7e
ddgilli <35736012+ddgilli@users.noreply.github.com>
2021-10-19 10:13:25 -0400

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -52,6 +52,7 @@ namespace API.Controllers
     public async Task<ActionResult<UserDto>> Login(LoginDto login)
     {
       var user = await _context.Users
+          .Include(p => p.Photos)
           .SingleOrDefaultAsync(x => x.UserName == login.Username.ToLower());
 
       if (user == null) return Unauthorized("Invalid username");
@@ -68,7 +69,8 @@ namespace API.Controllers
       return new UserDto
       {
           UserName = user.UserName,
-          Token = _tokenService.CreateToken(user)
+          Token = _tokenService.CreateToken(user),
+          PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
       };
 
     }
